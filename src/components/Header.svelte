ecb85ef0ffb05319aa5049ff49f98acdcf6fb344
Julian Chan <julianchan@cyberforce.com.hk>
2025-09-04 11:28:10 +0800

    starting commit for cloudflare

---
+++ src/components/Header.svelte
@@ -0,0 +1,4 @@
+<nav class="m-2 columns-1 flex-wrap font-story">
+	<a href="/" class="p-2 font-story">home</a>
+	<a href="/abstract" class="p-2 font-story">abstract</a>
+</nav>
---
+++ src/routes/abstract/+page.svelte
@@ -0,0 +1,7 @@
+<h1>about</h1>
+<p>this is the about page.</p>
+
+<div class="flex flex-wrap justify-around">
+	<p class="text-9xl">test</p>
+	<p class="text-9xl">test1</p>
+</div>
